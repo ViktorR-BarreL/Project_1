e2887e0750d0faf0580cba71417058b74c3f708e
ViktorR-BarreL <viktoriyabotchkareva@gmail.com>
2024-02-22 22:23:04 +0300

    ппп

--- main.py
+++ main.py
@@ -156,4 +156,4 @@ menu.mainloop()
 # Music promoted by https://www.chosic.com/free-music/all/
 # Creative Commons CC BY 3.0
 # https://creativecommons.org/licenses/by/3.0/
-# Проверка изменений2
+# Проверка изменений
