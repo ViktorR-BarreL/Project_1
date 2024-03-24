26fafe797d232dd6d476e499f9baf19aaf3af335
ViktorR-BarreL <viktoriyabotchkareva@gmail.com>
2024-03-24 15:58:23 +0300

    Update

--- main.py
+++ main.py
@@ -153,7 +153,6 @@ screen = Tk()
 
 w = screen.winfo_screenwidth()
 h = screen.winfo_screenheight()
-print(w, h)
 
 bg = Image.open('source/etc/bg.png')
 bg = bg.resize((w, h))
@@ -162,147 +161,75 @@ bg = ImageTk.PhotoImage(bg)
 screen.attributes('-fullscreen', True)
 screen.configure(bg='#96b3cf')
 
-body_img = Image.open('source/etc/body.png')
-body_img = body_img.resize((round(w * (body_img.width / 1920)), round(h * (body_img.height / 1080))))
-body_img = ImageTk.PhotoImage(body_img)
 
-features_img = Image.open('source/etc/features.png')
-features_img = features_img.resize((round(w * (features_img.width / 1920)),
-                                    round(h * (features_img.height / 1080))))
-features_img = ImageTk.PhotoImage(features_img)
+def process_image(path):
+    img = Image.open(path)
+    img = img.resize((round(w * (img.width / 1920)), round(h * (img.height / 1080))))
+    img = ImageTk.PhotoImage(img)
+    return img
 
-eyes_img = Image.open('source/etc/eye.png')
-eyes_img = eyes_img.resize((round(w * (eyes_img.width / 1920)), round(h * (eyes_img.height / 1080))))
-eyes_img = ImageTk.PhotoImage(eyes_img)
+# Категории выбора
+body_img = process_image('source/etc/body.png')
+features_img = process_image('source/etc/features.png')
+eyes_img = process_image('source/etc/eye.png')
+hair_img = process_image('source/etc/hair.png')
+top_img = process_image('source/etc/upp.png')
+bottom_img = process_image('source/etc/downn.png')
+socks_img = process_image('source/etc/socks.png')
+shoes_img = process_image('source/etc/shoes.png')
+glasses_img = process_image('source/etc/glasses.png')
+acc_img = process_image('source/etc/accs.png')
 
-hair_img = Image.open('source/etc/hair.png')
-hair_img = hair_img.resize((round(w * (hair_img.width / 1920)), round(h * (hair_img.height / 1080))))
-hair_img = ImageTk.PhotoImage(hair_img)
-
-top_img = Image.open('source/etc/upp.png')
-top_img = top_img.resize((round(w * (top_img.width / 1920)), round(h * (top_img.height / 1080))))
-top_img = ImageTk.PhotoImage(top_img)
-
-bottom_img = Image.open('source/etc/downn.png')
-bottom_img = bottom_img.resize((round(w * (bottom_img.width / 1920)),
-                                round(h * (bottom_img.height / 1080))))
-bottom_img = ImageTk.PhotoImage(bottom_img)
-
-socks_img = Image.open('source/etc/socks.png')
-socks_img = socks_img.resize((round(w * (socks_img.width / 1920)), round(h * (socks_img.height / 1080))))
-socks_img = ImageTk.PhotoImage(socks_img)
-
-shoes_img = Image.open('source/etc/shoes.png')
-shoes_img = shoes_img.resize((round(w * (shoes_img.width / 1920)), round(h * (shoes_img.height / 1080))))
-shoes_img = ImageTk.PhotoImage(shoes_img)
-
-glasses_img = Image.open('source/etc/glasses.png')
-glasses_img = glasses_img.resize((round(w * (glasses_img.width / 1920)), round(h * (glasses_img.height / 1080))))
-glasses_img = ImageTk.PhotoImage(glasses_img)
-
-acc_img = Image.open('source/etc/accs.png')
-acc_img = acc_img.resize((round(w * (acc_img.width / 1920)), round(h * (acc_img.height / 1080))))
-acc_img = ImageTk.PhotoImage(acc_img)
-
-color1 = Image.open('source/etc/color1.png')
-color1 = color1.resize((round(w * .04167), round(h * .0741)))
-color1 = ImageTk.PhotoImage(color1)
-
-color2 = Image.open('source/etc/color2.png')
-color2 = color2.resize((round(w * .04167), round(h * .0741)))
-color2 = ImageTk.PhotoImage(color2)
-
-color3 = Image.open('source/etc/color3.png')
-color3 = color3.resize((round(w * .04167), round(h * .0741)))
-color3 = ImageTk.PhotoImage(color3)
-
-color4 = Image.open('source/etc/color4.png')
-color4 = color4.resize((round(w * .04167), round(h * .0741)))
-color4 = ImageTk.PhotoImage(color4)
-
-color5 = Image.open('source/etc/color5.png')
-color5 = color5.resize((round(w * .04167), round(h * .0741)))
-color5 = ImageTk.PhotoImage(color5)
-
-color6 = Image.open('source/etc/color6.png')
-color6 = color6.resize((round(w * .04167), round(h * .0741)))
-color6 = ImageTk.PhotoImage(color6)
-
-color7 = Image.open('source/etc/color7.png')
-color7 = color7.resize((round(w * .04167), round(h * .0741)))
-color7 = ImageTk.PhotoImage(color7)
-
-color8 = Image.open('source/etc/color8.png')
-color8 = color8.resize((round(w * .04167), round(h * .0741)))
-color8 = ImageTk.PhotoImage(color8)
-
-color9 = Image.open('source/etc/color9.png')
-color9 = color9.resize((round(w * .04167), round(h * .0741)))
-color9 = ImageTk.PhotoImage(color9)
+# Цвета
+color1 = process_image('source/etc/color1.png')
+color2 = process_image('source/etc/color2.png')
+color3 = process_image('source/etc/color3.png')
+color4 = process_image('source/etc/color4.png')
+color5 = process_image('source/etc/color5.png')
+color6 = process_image('source/etc/color6.png')
+color7 = process_image('source/etc/color7.png')
+color8 = process_image('source/etc/color8.png')
+color9 = process_image('source/etc/color9.png')
 
 # Название Игры
-name = Image.open('source/etc/GameName.png')
-name = name.resize((round(w * .448), round(h * .495)))
-name = ImageTk.PhotoImage(name)
+name = process_image('source/etc/GameName.png')
 
 # Изображение для начала игры
-play = Image.open('source/etc/play.png')
-play = play.resize((round(w * .24583), round(h * .13056)))
-play = ImageTk.PhotoImage(play)
+play = process_image('source/etc/play.png')
 
 # Изображение выхода из игры
-ex = Image.open('source/etc/exit.png')
-ex = ex.resize((round(w * .068), round(h * .121)))
-ex = ImageTk.PhotoImage(ex)
+ex = process_image('source/etc/exit.png')
 
-# Изображение включения/выключения музыки
-sound_on = Image.open('source/etc/sound_on.png')
-sound_on = sound_on.resize((round(w * .068), round(h * .121)))
-sound_on = ImageTk.PhotoImage(sound_on)
-
-sound_off = Image.open('source/etc/sound_off.png')
-sound_off = sound_off.resize((round(w * .068), round(h * .121)))
-sound_off = ImageTk.PhotoImage(sound_off)
+# Изображения включения/выключения музыки
+sound_on = process_image('source/etc/sound_on.png')
+sound_off = process_image('source/etc/sound_off.png')
 
 # Элементы игрового окна
 
 # Выход в меню
-ex_menu = Image.open('source/etc/exmenu.png')
-ex_menu = ex_menu.resize((round(w * .0469), round(h * .0833)))
-ex_menu = ImageTk.PhotoImage(ex_menu)
+ex_menu = process_image('source/etc/exmenu.png')
 
 # Музыка
-sound_on1 = Image.open('source/etc/sound_onn.png')
-sound_on1 = sound_on1.resize((round(w * .0469), round(h * .0833)))
-sound_on1 = ImageTk.PhotoImage(sound_on1)
-
-sound_off1 = Image.open('source/etc/sound_offf.png')
-sound_off1 = sound_off1.resize((round(w * .0469), round(h * .0833)))
-sound_off1 = ImageTk.PhotoImage(sound_off1)
+sound_on1 = process_image('source/etc/sound_onn.png')
+sound_off1 = process_image('source/etc/sound_offf.png')
 
 # Рамка для предметов
-bg_menu = Image.open('source/etc/bgmenu.png')
-bg_menu = bg_menu.resize((round(w * .416), round(h * .767)))
-bg_menu = ImageTk.PhotoImage(bg_menu)
+bg_menu = process_image('source/etc/bgmenu.png')
 
 # Рамка для категорий
-bar_cat = Image.open('source/etc/barcat.png')
-bar_cat = bar_cat.resize((round(w * .4739), round(h * .1019)))
-bar_cat = ImageTk.PhotoImage(bar_cat)
+bar_cat = process_image('source/etc/barcat.png')
 
 # Рамка для цветов
-bar_col = Image.open('source/etc/barcol.png')
-bar_col = bar_col.resize((round(w * .0573), round(h * .7652)))
-bar_col = ImageTk.PhotoImage(bar_col)
+bar_col = process_image('source/etc/barcol.png')
 
 # Рамка для фона персонажа
-so_so = Image.open('source/etc/so.png')
-so_so = so_so.resize((round(w * .484), round(h * .963)))
-so_so = ImageTk.PhotoImage(so_so)
+so_so = process_image('source/etc/so.png')
 
-save = Image.open('source/etc/save.png')
-save = save.resize((round(w * .0469), round(h * .0833)))
-save = ImageTk.PhotoImage(save)
+# Кнопка сохранения
+save = process_image('source/etc/save.png')
+
+# Штуки для одевалки
+body_slot1 = process_image('source/1/etc/slot1.png')
 
 # Переменная для отслеживания состояния музыки
 music_playing = BooleanVar()
