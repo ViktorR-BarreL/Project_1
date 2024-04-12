ae0386924df2893da5477ef050495e35ffd98a78
ViktorR-BarreL <viktoriyabotchkareva@gmail.com>
2024-04-12 11:51:06 +0300

    Update

--- main.py
+++ main.py
@@ -411,8 +411,3 @@ buttons = []
 menu_on_screen()
 
 screen.mainloop()
-
-# Beach by Sakura Girl | https://soundcloud.com/sakuragirl_official
-# Music promoted by https://www.chosic.com/free-music/all/
-# Creative Commons CC BY 3.0
-# https://creativecommons.org/licenses/by/3.0/
